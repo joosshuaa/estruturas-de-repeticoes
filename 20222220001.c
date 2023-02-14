c85640b0d64dc0b2128a7cd13ebd4331e1351697
Josué Azevedo Gomes <111028817+joosshuaa@users.noreply.github.com>
2023-02-14 09:48:15 -0400

    Update 20222220001.c

--- 20222220001.c
+++ 20222220001.c
@@ -2,15 +2,30 @@
 
 /*Faça um programa que leia do teclado cinco números e imprima na tela 
 a soma destes cinco números. O programa só pode utilizar 2 (duas) variáveis.*/
-int main(){
-    int num;
-    int soma;
-    for(soma=1;soma<=5;soma++){
-        printf("numero %d: ", soma);
-        scanf("%d",&num);
-        soma += num;
-        printf("\nResultado da soma eh: ", soma);
-    }
-    
-    return 0;
+#include <stdio.h>
+
+int main() {
+  int num1, num2, soma;
+  printf("Digite o primeiro número: ");
+  scanf("%d", &num1);
+  soma = num1;
+
+  printf("Digite o segundo número: ");
+  scanf("%d", &num2);
+  soma += num2;
+
+  printf("Digite o terceiro número: ");
+  scanf("%d", &num2);
+  soma += num2;
+
+  printf("Digite o quarto número: ");
+  scanf("%d", &num2);
+  soma += num2;
+
+  printf("Digite o quinto número: ");
+  scanf("%d", &num2);
+  soma += num2;
+
+  printf("A soma dos cinco números é: %d\n", soma);
+  return 0;
 }

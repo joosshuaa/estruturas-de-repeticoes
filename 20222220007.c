5567071a54292a92a628cf1a1f0dcffd030d5ef7
Josué Azevedo Gomes <111028817+joosshuaa@users.noreply.github.com>
2023-02-14 09:47:08 -0400

    Update 20222220007.c

--- 20222220007.c
+++ 20222220007.c
@@ -1,43 +1,65 @@
 #include <stdio.h>
-/*Em uma disciplina com três exercícios, um professor deseja saber qual a maior
- e a menor nota de cada exercício e qual a maior e a menor média final. 
- Faça um programa para auxiliá-lo nesta tarefa.*/
-int main(){
-    float nota1;
-    float nota2;
-    float nota3;
-    float media;
-    float maior;
-    float menor;
-    printf("Digite a nota 1: ");
-    scanf("%f", &nota1);
-    printf("Digite a nota 2: ");
-    scanf("%f", &nota2);
-    printf("Digite a nota 3: ");
-    scanf("%f", &nota3);
 
-    if((nota1>=maior)&&(nota1>=menor)){
-        maior=nota1;
-        if ((nota1>=maior)&&(nota1>=menor)){
-            menor=nota1;
-        }       
-    }else if ((nota2>=maior)&&(nota2>=menor)){
-        maior=nota2;
-        if((nota2>=maior)&&(nota2>=menor)){
-            menor=nota2;
+#define NUM_EXERCICIO 3
+#define NUM_ESTUDANTE 10
+
+int main() {
+    int grades[NUM_EXERCICIO][NUM_ESTUDANTE];
+    int maxGradeExercise[NUM_EXERCICIO] = {0};
+    int minGradeExercise[NUM_EXERCICIO] = {0};
+    int maxFinalGrade = 0;
+    int minFinalGrade = 0;
+
+    // Input de notas
+    for (int i = 0; i < NUM_EXERCICIO; i++) {
+        for (int j = 0; j < NUM_ESTUDANTE; j++) {
+            printf("Informe a nota do aluno %d no exercicio %d: ", j + 1, i + 1);
+            scanf("%d", &grades[i][j]);
+
+            // Verifica maior nota do exercício
+            if (grades[i][j] > maxGradeExercise[i]) {
+                maxGradeExercise[i] = grades[i][j];
+            }
+
+            // Verifica menor nota do exercício
+            if (j == 0 || grades[i][j] < minGradeExercise[i]) {
+                minGradeExercise[i] = grades[i][j];
+            }
         }
-    }else if ((nota3>=maior)&&(nota3>=menor)){
-        maior=nota3;
-        if((nota3>=maior)&&(nota3>=menor)){
-            menor=nota3;
-        }       
     }
-    media = (nota1 + nota2 + nota3)/3;
-    printf("\n----------------------------------------------------\n");
-    printf("| Exercicio 1 | Exercicio 2| Exercicio 3 | Media     |\n");
-    printf("| maior|menor |maior|menor |maior|menor  |maior|menor| \n");
-    printf("| %1.2f |%1.2f  |%1.2f |%1.2f  |%1.2f |%1.2f   |%1.2f |%1.2f |\n",nota1, nota2, nota3, maior, menor, media );
-    printf("----------------------------------------------------\n");
+
+    // Calcula médias finais e verifica maior e menor média
+    for (int j = 0; j < NUM_ESTUDANTE; j++) {
+        int finalGrade = 0;
+        for (int i = 0; i < NUM_EXERCICIO; i++) {
+            finalGrade += grades[i][j];
+        }
+        finalGrade /= NUM_EXERCICIO;
+
+        // Verifica maior média final
+        if (finalGrade > maxFinalGrade) {
+            maxFinalGrade = finalGrade;
+        }
+
+        // Verifica menor média final
+        if (j == 0 || finalGrade < minFinalGrade) {
+            minFinalGrade = finalGrade;
+        }
+    }
+
+    // Imprime resultados
+    printf("Maior nota por exercicio:\n");
+    for (int i = 0; i < NUM_EXERCICIO; i++) {
+        printf("Exercicio %d: %d\n", i + 1, maxGradeExercise[i]);
+    }
+
+    printf("Menor nota por exercicio:\n");
+    for (int i = 0; i < NUM_EXERCICIO; i++) {
+        printf("Exercício %d: %d\n", i + 1, minGradeExercise[i]);
+    }
+
+    printf("Maior media final: %d\n", maxFinalGrade);
+    printf("Menor media final: %d\n", minFinalGrade);
 
     return 0;
 }
